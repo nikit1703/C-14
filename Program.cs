b47d514022b51aef53e0348f93bd1547fc2a76af
Nikita Pohelainen <demoniliangel@mail.ru>
2023-03-31 19:21:59 +0300

    itogovaya

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int size  = int.Parse(Console.ReadLine());
+
+string[] arr1 = new string[size];
+
+for (int i = 0; i < size; i++){
+	Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine();
+	arr1[i] = result;  
