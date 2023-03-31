6b4c98c9169e119f7eee7930b21ca04e1b508860
Nikita Pohelainen <demoniliangel@mail.ru>
2023-03-31 19:22:37 +0300

    itogovaya2

--- Program.cs
+++ Program.cs
@@ -7,3 +7,17 @@ for (int i = 0; i < size; i++){
 	Console.Write("Введите элемент массива: ");
     string result = Console.ReadLine();
 	arr1[i] = result;  
+    }
+Console.WriteLine();
+Console.Write("Введенный массив: [");
+Console.Write(string.Join(",", arr1));
+Console.Write("]");
+
+int count = 0;
+int maxSymbols = 3;
+
+for (int i = 0; i < arr1.Length; i++){
+	if(arr1[i].Length <= maxSymbols){
+		count++;
+	}   
+}
